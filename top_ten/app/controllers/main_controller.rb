3ee3e175fc87253a88876bc375b210fe55606b3a
KevinBurgin <Kevinburgin33@gmail.com>
2021-06-08 16:20:40 -0700

    intitial

---
+++ top_ten/app/controllers/main_controller.rb
@@ -0,0 +1,14 @@
+class MainController < ApplicationController
+    def topten
+     @one = "chilli mac"
+     @two = "meatball marinara"
+     @three = "cheese tortilini"
+     @four = "pizza"
+     @five = "chili with beans"
+     @six = "beef stew"
+     @seven = "beef taco"
+     @eight = "pork sausage"
+     @nine = "hashbrown"
+     @ten = "goulash"
+    end
+end
