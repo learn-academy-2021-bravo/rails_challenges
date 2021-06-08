bd6e0779c68ef0008dd761fe92bd89b6cdbebe0b
erik-engstrom <eekareeka@yahoo.com>
2021-06-08 15:57:43 -0700

    added 10 restaurants and links

--- rating_app/app/controllers/rating_controller.rb
+++ rating_app/app/controllers/rating_controller.rb
@@ -1,7 +1,15 @@
 class RatingController < ApplicationController
     def home
         @developers = ["Shazeen", "Erik"]
+        #  = ["Dija Mara", "Wench and Rodent", "Young and Pretty", "Campfire", "Market Restaurant and Bar", "Addison Restaurant", "A.R. Valentien", "Galaxy Taco", "Georges at the Cove", "Nine-Ten Restaurant and Bar"]
+        @rest_one = "Dija Mara"
+        @rest_two = "Wench and Rodent"
+        @rest_three = "Young and Pretty"
+        @rest_four = "Campfire"
+        @rest_five = "Market Restaurant and Bar"
         render 'home.html.erb'
     end
+
+
     
 end
