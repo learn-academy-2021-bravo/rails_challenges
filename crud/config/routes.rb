7c66a1bdd7c54ec901d33ecddc77d98a887404ed
Shazeen15 <shazzy.sterlin@gmail.com>
2021-06-09 15:51:56 -0700

    created a new and create

--- crud/config/routes.rb
+++ crud/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
   get '/posts' => 'post#index', as: 'posts'
+  get '/posts/new' => 'post#new', as:'new_post'
   get '/posts/:id' => 'post#show', as: 'post'
+  post '/posts' => 'post#create'
 
 end
